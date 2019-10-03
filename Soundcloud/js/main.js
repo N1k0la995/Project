d85416dfcba081bbcd62d6999994b2e9267bf942
N1k0la995 <46502036+N1k0la995@users.noreply.github.com>
2019-10-03 18:54:12 +0200

    Add files via upload

--- Soundcloud/js/main.js
+++ Soundcloud/js/main.js
@@ -1,19 +1,15 @@
-		function backward() 
-		{
+		function backward() {
             document.getElementById("forward").disabled = false;
         }
-		function CreateAccountCheckUsername()
-		{	
+		function createAccountCheckUsername(){	
 			document.getElementById("username1").innerHTML = "";
 			document.forms["FormCreateAccount"]["uname1"].style.border=" 1px solid gray ";
 		}
-		function CreateAccountCheckPassword()
-		{	
+		function createAccountCheckPassword(){	
 			document.getElementById("passw1").innerHTML = "";
 			document.forms["FormCreateAccount"]["password1"].style.border=" 1px solid gray ";
 		}
-		function CreateAccount() 
-		{
+		function createAccount() {
 			var username = document.forms["FormCreateAccount"]["uname1"].value;
 			var password = document.forms["FormCreateAccount"]["password1"].value;
 			document.getElementById("passw1").innerHTML =  "";
@@ -39,18 +35,15 @@
 				alert("Dear username:  "   +   username   +"  you successfully created account on Sound Cloud.");
 			}
 		}
-		function SingUpForFreeCheckUsername() 
-		{
+		function singUpForFreeCheckUsername() {
 			document.getElementById("username").innerHTML =  "";
 			document.forms["FormSingUpForFree"]["uname"].style.border="";
 		}
-		function SingUpForFreeCheckPassword() 
-		{
+		function singUpForFreeCheckPassword() {
 			document.getElementById("passw").innerHTML =  "";
 			document.forms["FormSingUpForFree"]["password"].style.border="";
 		}
-		function SingUpForFree() 
-		{
+		function singUpForFree() {
 				var username = document.forms["FormSingUpForFree"]["uname"].value;
 				var password = document.forms["FormSingUpForFree"]["password"].value;
 				document.getElementById("passw").innerHTML =  "";
@@ -76,8 +69,7 @@
 				alert("Welcome  username:  " + username +"  password:  "+ password +" ");
 			}
 		}
-		function RandomImage()
-		{
+		function randomImage() {
 			var images = ["image/head1.jpg","image/head2.jpg","image/head3.jpg"];
 			var size = images.length;
 			var x = Math.floor(size * Math.random());
@@ -85,44 +77,40 @@
 			document.getElementById("randomImageHeader").style.backgroundImage = 
 			"url("+images[x] +")";
 		}
-		document.addEventListener("DOMContentLoaded", RandomImage);
+		document.addEventListener("DOMContentLoaded", randomImage);
 		var createAccountModal= document.getElementById("CreateAccountModal");
-		function ButtonCreateAccountModal() 
-		{
+		function buttonCreateAccountModal() {
 			createAccountModal.style.display = "block";
 			document.body.style.overflow="hidden";
 		}
-		function CloseCreateAccount() 
-		{
+		function closeCreateAccount() {
 			createAccountModal.style.display = "none";
 			document.body.style.overflow="auto";
 		}
 		var singUpForFreeModal= document.getElementById("SingUpForFreeModal");
-		function ButtonSingUpForFreeModal() 
-		{
+		function buttonSingUpForFreeModal() {
 			singUpForFreeModal.style.display = "block";
 			document.body.style.overflow="hidden";
 		}
-		function CloseSingUpForFreeModal() 
-		{
+		function closeSingUpForFreeModal() {
 			singUpForFreeModal.style.display = "none";
 			document.body.style.overflow="auto";
 		}
 		var language = document.getElementById("Language");
-		function OpenLanguageModal() {
+		function openLanguageModal() {
 			language.style.display = "block";
 			document.body.style.overflow="hidden";
 		}
-		function CloseLanguageModal() {
+		function closeLanguageModal() {
 			language.style.display = "none";
 			document.body.style.overflow="auto";
 		}
-		function CloseButtonLanguageModal(){
+		function closeButtonLanguageModal(){
 			language.style.display = "none";
 			document.body.style.overflow="auto";
 		}
 		window.onclick = function(event) {
-			if (event.target == language ||	event.target == singUpForFreeModal ||	event.target == createAccountModal ) {
+			if (event.target == language ||	event.target == singUpForFreeModal || event.target == createAccountModal ) {
 				language.style.display = "none";
 				singUpForFreeModal.style.display = "none";
 				createAccountModal.style.display = "none";
@@ -142,7 +130,6 @@
 				.then(function() { console.log("GAPI client loaded for API"); },
 					function(err) { console.error("Error loading GAPI client for API", err); });
 		};	
-
 		// Make sure the client is loaded and sign-in is complete before calling this method.
 		function execute() {
 			return gapi.client.youtube.playlistItems.list({
@@ -161,45 +148,44 @@
 				const position =item.snippet.position;
 				output +=  `
 				<div class="col-2">
-				<img class="VideoImage" value="${position}" id="img${position}" onclick="YouTubeVideo(this)" src=" https://img.youtube.com/vi/${videoId}/hqdefault.jpg" style=" position:absolute; ">
+				<img class="VideoImage" value="${position}" id="img${position}" onclick="removeImageAndPlayYouTubeVideo(this)" src=" https://img.youtube.com/vi/${videoId}/hqdefault.jpg" style=" position:absolute; ">
 				<iframe class="IFrameYoutube"  id="ifr${position}"  src="https://www.youtube.com/embed/${videoId}" frameborder="0"  allow="autoplay; encrypted-media"   ></iframe>
-			<p id="vid${position}" style="display:none;">https://www.youtube.com/embed/${videoId}</p>
-			</div>`;
+				<p id="vid${position}" style="display:none;">https://www.youtube.com/embed/${videoId}</p>
+				</div>`;
 			});
 			videoContainer.innerHTML=output;
 			},
 
 			function(err) { console.error("Execute error", err); });
 		};			
-		
-
 		gapi.load("client:auth2", function() {
 			gapi.auth2.init({client_id: "272099904269-22euf40hdr21i671lgjrsrgispea40cg.apps.googleusercontent.com"});
 		})
-		var y=document.getElementsByClassName("VideoImage");
-		var z=document.getElementsByClassName("IFrameYoutube");
-		var k;
-		var x;
-
-		function YouTubeVideo(e) {
-			for(var i = 0; i < y.length; i++){
-				if (y[i] !== this) {
-					y[i].style.display = "block";  
+		var imageList=document.getElementsByClassName("VideoImage");
+		var videoList=document.getElementsByClassName("IFrameYoutube");
+		var imageClickValue;
+		var controlPanel=0;
+		function removeImageAndPlayYouTubeVideo(value) {
+			for(var i = 0; i < imageList.length; i++){
+				if (imageList[i] !== this) {
+					imageList[i].style.display = "block";  
 				}
-				e.style.display = "none";
-				k = e.getAttribute("value"); 
-				document.getElementById("demo").innerHTML = k;
-
-
+				value.style.display = "none";
+				imageClickValue = value.getAttribute("value"); 
 			}
-			for(var u=0; u < z.length; u++){
-				if (u !== k){
-					x[u] = document.getElementById("vid"+u+"").innerHTML;
-					document.getElementById("ifr"+u+"").src = x[u];
+			for(var u = 0; u < videoList.length; u++){
+				if (u != imageClickValue) {
+					videoList[u].src= document.getElementById("vid" + u).innerHTML;
 				}
-				else {
-					document.getElementById("ifr"+u+"").src +="?autoplay=1";
+				else{
+					document.getElementById("ifr" + imageClickValue).src +="?autoplay=1";
 				}
 			}
-
+			if (controlPanel==1){
+				var cont =	document.getElementById("control");
+				cont.style.display = "block";
+			}
+			else{
+				controlPanel=controlPanel+1;
+			}
 		};
